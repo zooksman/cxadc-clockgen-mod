41d505d3e8f4a9b4b77d7cf2d12fe07798facf52
zooksman <zooksman@gmail.com>
2025-11-26 20:42:22 -0600

    Increase clock GPIO pin drive strength to decrease clock jitter and rise time.

--- firmware/src/clock_gen.c
+++ firmware/src/clock_gen.c
@@ -6,6 +6,7 @@
 
 #include <hardware/clocks.h>
 #include <pico/stdlib.h>
+#include <hardware/gpio.h>
 
 #define adc_rate_40mhz 78125
 
@@ -17,6 +18,9 @@ bool clock_gen_init()
     //set_sys_clock_khz(120000, true);
     // Rather than underclock the pico, slightly overclock to 160MHz (up to 200Mhz is officially supported)
     set_sys_clock_khz(160000, true);
+    
+    // To help keep a stable clock on longer lines, increase GPIO drive strength from 4ma to 8ma
+    gpio_set_drive_strength(CLOCK_PIN, GPIO_DRIVE_STRENGTH_8MA);
     
     // Then, divide by 4 instead of 3 to achieve an even 40MHz
     //clock_gpio_init_int_frac(CLOCK_PIN, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 3, 0);
